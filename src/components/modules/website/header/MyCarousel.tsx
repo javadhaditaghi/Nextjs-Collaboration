34e628d13cc2bc807bd9f96d38325a9811535b67
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-10-16 13:41:45 +0330

    chang slidebar style with scrollbar module

--- src/components/modules/website/header/MyCarousel.tsx
+++ src/components/modules/website/header/MyCarousel.tsx
@@ -1,66 +1,68 @@
 'use client'
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Image from 'next/image'
+
+import React from 'react';
+import Image from 'next/image';
 import Text from '@/components/Text';
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function MyCarousel() {
   return (
     <>
-    
     <Swiper
-    
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>
-        <Image
-      src="/assets/images/bg.png"
-      layout='responsive'
-      width={1920}
-      height={800}
-      alt="Picture of the author"
-    />
-    </SwiperSlide>
-
+    // install Swiper modules
+    modules={[Navigation, Pagination, A11y]}
+  
+    slidesPerView={1}
+    navigation
+    pagination={{ clickable: true }}
+    scrollbar={{ draggable: true }}
+    onSwiper={(swiper) => console.log(swiper)}
+    onSlideChange={() => console.log('slide change')}
+  >
     <SwiperSlide>
-        <Image
+    <Image
       src="/assets/images/bg.png"
       layout='responsive'
       width={1920}
-      height={1080}
+      height={2000}
       alt="Picture of the author"
     />
     </SwiperSlide>
-
-      <SwiperSlide>
-        <Image
+    <SwiperSlide>
+    <Image
       src="/assets/images/bg.png"
       layout='responsive'
       width={1920}
-      height={1080}
+      height={2000}
       alt="Picture of the author"
     />
     </SwiperSlide>
-
-      <SwiperSlide>
-        <Image
+    <SwiperSlide>
+    <Image
       src="/assets/images/bg.png"
       layout='responsive'
       width={1920}
-      height={1080}
+      height={2000}
       alt="Picture of the author"
     />
     </SwiperSlide>
-      ...
-    </Swiper>
-    <Text  />
-    </>
-  
+    
+    ...
+  </Swiper>
+  <Text />
+  </>
   )
-  
 }
 
 export default MyCarousel
+
