9bba4bace72523d2d97ee14fd9615e15b9a22c88
javad <javadht1990a@gmail.com>
2024-10-25 01:06:40 +0330

    minor changes

--- src/components/modules/website/header/Header.tsx
+++ src/components/modules/website/header/Header.tsx
@@ -2,17 +2,18 @@
 
 import MyAppBar from "./MyAppBar"
 import MyCarousel from "./MyCarousel"
+import { Box } from "@mui/material"
 
 
 
 const Header = () => {
 
     return (
-        <>
+        <Box sx={{marginBottom:"130px"}}>
       
         <MyAppBar />
         <MyCarousel />
-        </>
+        </Box>
     )
 
 
